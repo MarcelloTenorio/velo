8d238f7eb22ce09639202b1bd6defb383fd3a66d
Marcello Tenorio <marcellohtenorio@gmail.com>
2026-01-25 17:27:43 +0000

    AAA - Arrange, Act e Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,9 +1,14 @@
   //Teste automatizado de consulta de pedidos, usando o "codegen" do playwright
 import { test, expect } from '@playwright/test'
 
-test('consulta de pedidos', async ({ page }) => {
-  await page.goto('http://localhost:5173/')
+/// AAA - Arrange, Act, Assert
+/// PAV - Preparar, Agir, Validar
 
+test('consulta de pedidos', async ({ page }) => {
+  //Arrange
+  //Checkpoint: Verificar se a webapp está online
+  await page.goto('http://localhost:5173/')
+  
   //Checkpoint: Verificar o titulo da página
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
   
@@ -13,10 +18,17 @@ test('consulta de pedidos', async ({ page }) => {
   //Checkpoint: Verificar o titulo da página de consulta de pedidos
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-  //Preenchimento do campo de busca de pedidos
+  
+  // -----------------------------------------------------------------------------------
+  //Act
+  // Preencher o campo de busca de pedidos
   await page.getByTestId('search-order-id').fill('VLO-VA9DJ2')
+  //Act - Clicar no botão de buscar pedido
   await page.getByTestId('search-order-button').click()
 
+
+  // -----------------------------------------------------------------------------------
+  //Assert
   //Checkpoint: Verificar o resultado da busca de pedidos
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-VA9DJ2')
