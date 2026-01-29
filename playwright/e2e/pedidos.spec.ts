38af2275942a5ac7e299023c268d369ada2a4c0d
Marcello Tenorio <marcellohtenorio@gmail.com>
2026-01-29 23:33:00 +0000

    OrderLookup Sem DataTestID

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -22,18 +22,37 @@ test('consulta de pedidos', async ({ page }) => {
   // -----------------------------------------------------------------------------------
   //Act
   // Preencher o campo de busca de pedidos
-  await page.getByTestId('search-order-id').fill('VLO-VA9DJ2')
+  await page.locator('//label[text()="Número do Pedido"]/..//input').fill('VLO-VA9DJ2')  //XPath para achar o campo de busca de pedidos usando o label do input e a div pai
+  //await page.getByTestId('search-order-id').fill('VLO-VA9DJ2')
+  // await page.getByPlaceholder('Ex: VLO-ABC123').fill('VLO-VA9DJ2')
+  // await page.getByLabel('Número do Pedido').fill('VLO-VA9DJ2')
+  // await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-VA9DJ2')
+
   //Act - Clicar no botão de buscar pedido
-  await page.getByTestId('search-order-button').click()
+  //await page.locator('//button[text()="Buscar Pedido"]').click()
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
 
   // -----------------------------------------------------------------------------------
   //Assert
   //Checkpoint: Verificar o resultado da busca de pedidos
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-VA9DJ2')
+  
+  //await page.waitForTimeout(30000)  <- Nesse exemplo de uso do timeout, a automação vai esperar 30 segundos para o elemento ser visível, ou seja, vai dormir por 30 segundos
+
+      // Resolução Desafio
+  //Primeira forma (Com locator e Xpath)
+  //await expect(page.locator('//p[text()="VLO-VA9DJ2"]/..//p')).toBeVisible({timeout: 30_000})
+  //await expect(page.locator('//p[text()="VLO-VA9DJ2"]/..//p')).toContainText('VLO-VA9DJ2')
+  //Segunda forma (Com GetByText e 'nome no elemento')
+  await expect(page.getByText('VLO-VA9DJ2')).toBeVisible({timeout: 30_000})  //<- Aqui o timeout vai tentar buscar até 30 segundos.
+  await expect(page.getByText('VLO-VA9DJ2')).toContainText('VLO-VA9DJ2')
+
 
   //Checkpoint: Verificar o status do pedido
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  //Primeira forma (Com locator e Xpath)
+  //await expect(page.locator('//div[text()="APROVADO"]')).toBeVisible()
+  //await expect(page.locator('//div[text()="APROVADO"]')).toContainText('APROVADO')
+  //Segunda forma (Com GetByText e 'nome no elemento')
+  await expect(page.getByText('APROVADO')).toBeVisible()
+  await expect(page.getByText('APROVADO')).toContainText('APROVADO')
 })
