f1d579232bbc1294ec6cc000c737892cfed1077b
Marcello Tenorio <marcellohtenorio@gmail.com>
2026-05-01 08:18:11 +0100

    Como um SDET evita testes quebradiços

--- playwright/support/actions/checkoutActions.ts
+++ playwright/support/actions/checkoutActions.ts
@@ -3,11 +3,22 @@ import { Page, expect } from '@playwright/test'
 export function createCheckoutActions(page: Page) {
 
   const terms = page.getByTestId('checkout-terms')
+  const alerts = {
+    name: page.getByTestId('error-name'),
+    lastname: page.getByTestId('error-lastname'),
+    email: page.getByTestId('error-email'),
+    phone: page.getByTestId('error-phone'),
+    document: page.getByTestId('error-document'),
+    store: page.getByTestId('error-store'),
+    terms: page.getByTestId('error-terms')
+  }
+
 
   return {
 
     elements: {
-      terms
+      terms,
+      alerts
     },
 
     async expectLoaded() {
@@ -25,10 +36,10 @@ export function createCheckoutActions(page: Page) {
       document: string
     }) {
       await page.getByTestId('checkout-name').fill(data.name)
-      await page.getByTestId('checkout-surname').fill(data.lastname)
+      await page.getByTestId('checkout-lastname').fill(data.lastname)
       await page.getByTestId('checkout-email').fill(data.email)
       await page.getByTestId('checkout-phone').fill(data.phone)
-      await page.getByTestId('checkout-cpf').fill(data.document)
+      await page.getByTestId('checkout-document').fill(data.document)
     },
 
     async selectStore(storeName: string) {
