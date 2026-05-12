593aba8926972e07418a39575d0905b04f168011
Marcello Tenorio <marcellohtenorio@gmail.com>
2026-05-12 16:13:15 +0100

    Fluxo E2E + Code Challenge: controle de dados no banco

--- playwright/e2e/checkout.spec.ts
+++ playwright/e2e/checkout.spec.ts
@@ -1,16 +1,14 @@
 import { test, expect } from '../support/fixtures'
+import { deleteOrderByDocument } from '../support/database/orderRepository'
 
 test.describe('Checkout', () => {
 
-    test.beforeEach(async ({ page }) => {
-        await page.goto('/order')
-        await expect(page.getByRole('heading', { name: 'Finalizar Pedido' })).toBeVisible()
-    })
-
-
     test.describe('Validações de campos obrigatórios', () => {
         let alerts: any
-        test.beforeEach(async ({ app }) => {
+
+        test.beforeEach(async ({ app, page }) => {
+            await page.goto('/order')
+            await expect(page.getByRole('heading', { name: 'Finalizar Pedido' })).toBeVisible()
             alerts = app.checkout.elements.alerts
         })
 
@@ -116,6 +114,46 @@ test.describe('Checkout', () => {
 
             // Assert
             await expect(alerts.terms).toHaveText('Aceite os termos')
+        })
+    })
+
+
+    test.describe('Pagamento e Confirmação', () => {
+
+        test('deve criar um pedido com sucesso para pagamento à vista', async ({ app, page }) => {
+            const customer = {
+                name: 'João',
+                lastname: 'Silva',
+                email: 'joaosilva@email.com',
+                document: '529.982.247-25',
+                phone: '(11) 99999-9999',
+                store: 'Velô Paulista',
+                paymentMethod: 'À vista',
+                totalPrice: 'R$ 40.000,00'
+            }
+
+            await deleteOrderByDocument(customer.document)
+
+            // Arrange
+            await page.goto('/')
+            await page.getByRole('link', { name: /Configure Agora/i }).click()
+
+            await app.configurator.expectPrice("R$ 40.000,00")
+            await app.configurator.finishConfigurator()
+            await app.checkout.expectLoaded()
+
+            await app.checkout.fillCustomerData(customer)
+            await app.checkout.selectStore(customer.store)
+
+            // Act
+            await app.checkout.selectPaymentMethod(customer.paymentMethod)
+            await app.checkout.expectSummaryTotal(customer.totalPrice)
+            await app.checkout.acceptTerms()
+            await app.checkout.submit()
+
+            // Assert
+            await expect(page).toHaveURL(/\/success/)
+            await expect(page.getByRole('heading', { name: 'Pedido Aprovado' })).toBeVisible()
         })
     })
 })
